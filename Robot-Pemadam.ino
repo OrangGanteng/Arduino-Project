be2fec5cbff1dbe11bc7e7e4fbec233a49963234
OrangGanteng <49611257+OrangGanteng@users.noreply.github.com>
2025-08-06 22:32:26 +0700

    Update Robot-Pemadam.ino

--- Robot-Pemadam.ino
+++ Robot-Pemadam.ino
@@ -1,31 +1,20 @@
 #include <IRremote.hpp>
 #include <AFMotor.h>
 
-
-Servo myservo;
-
 #define IR_RECEIVE_PIN 9 // pin SERVO_2
-#define pin_ngencrit 10
+#define pompa 10
+
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 
-int pwm = 150;
-
-const int kode_maju = 3877175040;
-const int kode_mundur = 2907897600;
-const int kode_belok_kanan = 2774204160;
-const int kode_belok_kiri = 4144561920;
-const int kode_berhenti = 3810328320;
-
-const int kode_ngencrit = 3910598400;
-const int kode_ngencrit_tutup = 4061003520;
-
-const int servo_tutup = 35; // servo 0 derajat
+int pwm = 150; 
 
 void setup() {
   IrReceiver.begin(IR_RECEIVE_PIN); // pin SERVO_1 motorshield
+    
+  pinMode(pompa, OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
@@ -43,152 +32,87 @@ void loop() {
       IrReceiver.resume();
       break;
     case 2774204160:
-      belok_kanan();
+      kanan();
       delay(5);
       IrReceiver.resume();
       break;
     case 4144561920:
-      belok_kiri();
+      kiri();
       IrReceiver.resume();
       delay(5);
       break;
     case 3810328320:
-      berhenti();
+      Stop();
       delay(5);
       IrReceiver.resume();
       break;
     case 3910598400:
-      digitalWrite(pin_ngencrit, HIGH);
+      digitalWrite(pompa, HIGH);
       delay(5000);
       IrReceiver.resume();
       break;
-    
+   
     case 4061003520:
-      digitalWrite(pin_ngencrit, LOW);
+      digitalWrite(pompa, LOW);
       IrReceiver.resume();
       break;
 }
 }
 }
 void maju() {
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(pwm);
-  motor3.setSpeed(pwm);
-  motor4.setSpeed(pwm);
+  motor1.setSpeed(200);
+  motor2.setSpeed(200);
+  motor3.setSpeed(200);
+  motor4.setSpeed(200);
   motor1.run(FORWARD);
-  motor3.run(FORWARD);
+  motor2.run(FORWARD);
   motor3.run(FORWARD);
   motor4.run(FORWARD);
 }
 void mundur() {
 
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(pwm);
-  motor3.setSpeed(pwm);
-  motor4.setSpeed(pwm);
+  motor1.setSpeed(200);
+  motor2.setSpeed(200);
+  motor3.setSpeed(200);
+  motor4.setSpeed(200);
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
   motor3.run(BACKWARD);
   motor4.run(BACKWARD);
 }
-void belok_kanan() {
-
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(pwm);
-  motor3.setSpeed(pwm);
-  motor4.setSpeed(pwm);
-  motor1.run(BACKWARD);
-  motor2.run(FORWARD);
-  motor3.run(BACKWARD);
-  motor4.run(FORWARD);
+void kanan()
+{
+  motor1.setSpeed(255); 
+  motor1.run(FORWARD); 
+  motor2.setSpeed(255); 
+  motor2.run(BACKWARD); 
+  motor3.setSpeed(255); 
+  motor3.run(FORWARD);  
+  motor4.setSpeed(255); 
+  motor4.run(BACKWARD);  
 }
-void belok_kiri() {
 
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(pwm);
-  motor3.setSpeed(pwm);
-  motor4.setSpeed(pwm);
-  motor2.run(FORWARD);
-  motor1.run(BACKWARD);
-  motor4.run(FORWARD);
-  motor3.run(BACKWARD);
+void kiri()
+{
+  motor1.setSpeed(255);
+  motor1.run(BACKWARD);  
+  motor2.setSpeed(255); 
+  motor2.run(FORWARD);  
+  motor3.setSpeed(255); 
+  motor3.run(BACKWARD); 
+  motor4.setSpeed(255); 
+  motor4.run(FORWARD); 
 }
-void putar_kiri() {
-
-  motor.setSpeed(pwm);
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(pwm);
-  motor3.setSpeed(pwm);
-  motor2.run(FORWARD);
-  motor1.run(FORWARD);
-  motor4.run(BACKWARD);
-  motor3.run(BACKWARD);
-}
-void putar_kanan() {
-
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(pwm);
-  motor3.setSpeed(pwm);
-  motor4.setSpeed(pwm);
-  motor2.run(BACKWARD);
-  motor1.run(BACKWARD);
-  motor4.run(FORWARD);
-  motor3.run(FORWARD);
-}
-void moveRightForward() {
-
-  motor.setSpeed(pwm);
-  motor1.setSpeed(0);
-  motor2.setSpeed(pwm);
-  motor3.setSpeed(0);
-  motor4.run(RELEASE);
-  motor2.run(RELEASE);
-  motor1.run(FORWARD);
-  motor3.run(FORWARD);
-}
-void moveRightBackward() {
-
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(0);
-  motor3.setSpeed(pwm);
-  motor4.setSpeed(0);
+void Stop()
+{
+  motor1.setSpeed(0); 
   motor1.run(RELEASE);
-  motor3.run(RELEASE);
-  motor2.run(BACKWARD);
-  motor4.run(BACKWARD);
-
-}
-void moveLeftForward() {
-
-  motor.setSpeed(0);
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(0);
-  motor3.setSpeed(pwm);
-  motor3.run(RELEASE);
-  motor1.run(RELEASE);
-  motor2.run(FORWARD);
-  motor4.run(FORWARD);
-}
-void moveLeftBackward() {
-
-  motor1.setSpeed(pwm);
-  motor2.setSpeed(0);
-  motor3.setSpeed(pwm);
-  motor4.setSpeed(0);
-  motor3.run(RELEASE);
-  motor2.run(RELEASE);
-  motor1.run(BACKWARD);
-  motor4.run(BACKWARD);
-}
-void berhenti() {
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  motor3.run(RELEASE);
-  motor4.run(RELEASE);
-  motor1.setSpeed(0);
-  motor2.setSpeed(0);
-  motor3.setSpeed(0);
-  motor4.setSpeed(0);
+  motor2.setSpeed(0);  
+  motor2.run(RELEASE); 
+  motor3.setSpeed(0);  
+  motor3.run(RELEASE); 
+  motor4.setSpeed(0);  
+  motor4.run(RELEASE); 
 }
 
 
